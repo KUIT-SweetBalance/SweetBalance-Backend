cf0e45fe87dc96a1f8615737944f10a7d7cd0f10
kisusu <kikisu115@naver.com>
2025-01-22 15:08:48 +0900

    feat: 도메인 수정 이후 secure, samesite 설정 반영

--- src/main/java/com/sweetbalance/backend/util/filter/LoginFilter.java
+++ src/main/java/com/sweetbalance/backend/util/filter/LoginFilter.java
@@ -76,10 +76,16 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         //응답 설정
         response.setHeader("Authorization", "Bearer " + accessToken);
-        response.addCookie(createCookie("refresh", refreshToken));
+        setRefreshTokenCookie(response, refreshToken);
 
         InnerFilterResponseSender.sendInnerResponse(response, 200, 0,
                 "로그인 성공, 토큰 발급 성공", null);
+    }
+
+    private void setRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
+        String cookieValue = String.format("refresh=%s; Max-Age=%d; Path=/; Domain=localhost; Secure; SameSite=None",
+                refreshToken, 24*60*60);
+        response.setHeader("Set-Cookie", cookieValue);
     }
 
     //로그인 실패시 실행하는 메소드
@@ -88,17 +94,5 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         InnerFilterResponseSender.sendInnerResponse(response, 400, 999,
                 "로그인 인증 실패", null);
-    }
-
-    private Cookie createCookie(String key, String value) {
-
-        Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24*60*60);
-        cookie.setPath("/");
-        cookie.setDomain("localhost");
-        //cookie.setHttpOnly(true);
-        //cookie.setSecure(true);
-
-        return cookie;
     }
 }
