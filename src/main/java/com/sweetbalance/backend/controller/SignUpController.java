c438f173f7c9f10a39cdd37bc6491e7da6d7bc4c
kisusu <kikisu115@naver.com>
2025-01-27 19:05:25 +0900

    feat: 회원가입 API (/api/auth/sign-up) request body 에 nickname과 gender를 포함하여 요청받도록 수정

--- src/main/java/com/sweetbalance/backend/controller/SignUpController.java
+++ src/main/java/com/sweetbalance/backend/controller/SignUpController.java
@@ -5,6 +5,8 @@ import com.sweetbalance.backend.dto.request.SignUpRequestDTO;
 import com.sweetbalance.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,16 @@ public class SignUpController {
                     DefaultResponseDTO.error(400, 999, "중복된 username 또는 email")
             );
         }
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        String errorMessage = "잘못된 입력 형식입니다.";
+        if (e.getMessage().contains("Gender")) {
+            errorMessage = "성별은 'MALE' 또는 'FEMALE'만 가능합니다.";
+        }
+        return ResponseEntity.status(400).body(
+                DefaultResponseDTO.error(400, 999, errorMessage)
+        );
     }
 }
