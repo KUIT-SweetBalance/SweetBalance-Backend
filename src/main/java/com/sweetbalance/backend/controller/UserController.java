d27a9a26a398edeea6a9ae657f045e0d2f044349
yskim6772 <yskim6772@gmail.com>
2025-02-07 04:45:29 +0900

    #32 [add] : 오늘 영양정보 섭취 가능한 당 양 데이터 추가

--- src/main/java/com/sweetbalance/backend/controller/UserController.java
+++ src/main/java/com/sweetbalance/backend/controller/UserController.java
@@ -9,6 +9,7 @@ import com.sweetbalance.backend.entity.Beverage;
 import com.sweetbalance.backend.entity.BeverageLog;
 import com.sweetbalance.backend.entity.BeverageSize;
 import com.sweetbalance.backend.entity.User;
+import com.sweetbalance.backend.enums.user.Gender;
 import com.sweetbalance.backend.service.BeverageService;
 import com.sweetbalance.backend.service.BeverageSizeService;
 import com.sweetbalance.backend.service.UserService;
@@ -186,6 +187,8 @@ public class UserController {
             );
         }
 
+        User user = userOptional.get();
+
         List<BeverageLog> dailyBeverageLogs = userService.findTodayBeverageLogsByUserId(userId);
 
         double initSugarSum = 0.0;
@@ -199,8 +202,16 @@ public class UserController {
 
         int unreadAlarmCount = userService.getNumberOfUnreadLogWithinAWeek();
 
+        int additionalSugar = 0;
+        if (user.getGender() == Gender.MALE) {
+            additionalSugar = 25 - totalSugar;
+        } else if (user.getGender() == Gender.FEMALE) {
+            additionalSugar = 20 - totalSugar;
+        }
+
         DailyConsumeInfoDTO dailyConsumeInfo = DailyConsumeInfoDTO.builder()
                 .totalSugar(totalSugar)
+                .additionalSugar(additionalSugar)
                 .beverageCount(beverageCount)
                 .unreadAlarmCount(unreadAlarmCount)
                 .build();
