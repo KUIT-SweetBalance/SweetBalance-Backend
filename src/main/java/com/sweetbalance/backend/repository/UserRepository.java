27f9bac4fa673d932216dec5332fbade403cf5da
kisusu <kikisu115@naver.com>
2025-02-07 18:28:10 +0900

    #28 [feat] : soft deleted 된 User에 대해 검증을 진행하지 않도록 기존 로직 변경 - OAuth2 User

--- src/main/java/com/sweetbalance/backend/repository/UserRepository.java
+++ src/main/java/com/sweetbalance/backend/repository/UserRepository.java
@@ -1,25 +1,16 @@
 package com.sweetbalance.backend.repository;
 
-import com.sweetbalance.backend.entity.Beverage;
 import com.sweetbalance.backend.entity.User;
-import com.sweetbalance.backend.enums.common.Status;
 import com.sweetbalance.backend.enums.user.LoginType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByEmailAndLoginType(String email, LoginType loginType);
-
-    Optional<User> findByEmailAndLoginType(String email, LoginType loginType);
-
     Optional<User> findByEmailAndLoginTypeAndDeletedAtIsNull(String email, LoginType loginType);
 
-    Optional<User> findByProviderIdAndStatus(String providerId, Status status);
+    Optional<User> findByProviderIdAndDeletedAtIsNull(String providerId);
 }
