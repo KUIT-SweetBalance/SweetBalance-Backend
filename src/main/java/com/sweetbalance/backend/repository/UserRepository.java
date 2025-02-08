0636c2de8662dd9f4ef7b02f67c1bf36cc208b09
yskim6772 <yskim6772@gmail.com>
2025-02-09 00:13:07 +0900

    #26 [fix] : UserRepository 충돌 해결

--- src/main/java/com/sweetbalance/backend/repository/UserRepository.java
+++ src/main/java/com/sweetbalance/backend/repository/UserRepository.java
@@ -14,13 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByProviderIdAndDeletedAtIsNull(String providerId);
 
-    // 사용자 이름으로 조회
-    Optional<User> findByUsername(String username);
-
-    // email로 사용자 조회 (임시 비밀번호 발급용)
     Optional<User> findByEmail(String email);
-
-    // 특정 사용자 ID로 Beverage 목록 조회
-    @Query("SELECT b FROM BeverageLog bl JOIN bl.beverageSize b WHERE bl.user.userId = :userId")
-    List<Beverage> findBeveragesByUserId(@Param("userId") Long userId);
 }
