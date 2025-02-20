b5078c9c881ac7fb3a10dcf8f9b01ce69a72b3ae
seungwoo1209 <2002msw@naver.com>
2025-02-21 02:40:44 +0900

    [refactor]: print문 제거

--- src/main/java/com/sweetbalance/backend/service/NoticeServiceImpl.java
+++ src/main/java/com/sweetbalance/backend/service/NoticeServiceImpl.java
@@ -40,7 +40,6 @@ public class NoticeServiceImpl implements NoticeService{
 
         Map<Long, Alarm> alarmByLogId = getAlarmMapWithin(userId,oneWeekAgo, now);
         List<BeverageLog> sortedLogs = getSortedLogsWithin(userId, oneWeekAgo, now);
-        sortedLogs.forEach(System.out::println);
         List<BaseEntity> integratedLogs = integrateLogsAndAlarmsToList(sortedLogs, alarmByLogId);
         List<EachNotice> noticeList = convertToNoticeDTOFormat(integratedLogs, dateTimeFormatter);
         Map<String, List<EachNotice>> groupedMap = groupNoticeListByDate(noticeList, dateTimeFormatter, dateFormatter, timeFormatter);
@@ -182,7 +181,6 @@ public class NoticeServiceImpl implements NoticeService{
 
         // 알람의 위치는 어디가 적절한가?
         List<Pair<Long, SugarWarning>> properAlarmList = getProperAlarmLocation(user.getGender(), logsOnSameDay, (double)cautionAmountOfSugar, (double)exceedAmountOfSugar);
-        properAlarmList.forEach(p -> System.out.println(p.getLeft() + ", " + p.getRight()));
         int size1 = alarmsOnSameDay.size();
         int size2 = properAlarmList.size();
         int max_size = Math.max(size1, size2);
@@ -249,7 +247,6 @@ public class NoticeServiceImpl implements NoticeService{
         for (BeverageLog bl : logsOnSameDay) {
             // 해당 날에 섭취한 당 함량
             accumulatedSugar += bl.getBeverageSize().getSugar() + bl.getAdditionalSugar();
-            System.out.println("accumulatedSugar = " + accumulatedSugar);
             // 한번에 초과했을 경우(주의 알람이 필요 없음) 또는 주의 알람 후 당 함량을 초과했을 경우
             if(accumulatedSugar >= exceedAmountOfSugar){
                 properAlarm.add(Pair.of(bl.getLogId(),SugarWarning.EXCEED));
